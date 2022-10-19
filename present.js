e0fa30f7342cef05acde5db6809bdc932e239092
Huankento <116175143+Huankento@users.noreply.github.com>
2022-10-19 20:06:27 +0700

    Add files via upload

---
+++ present.js
@@ -0,0 +1,46 @@
+
+var to = 'Các cô gái 11B1';
+var gift_url = '4464243257440465093.mp4';
+var gift_image_url = 'z3810736486215_f6d974e473e0934b8de78982e19686f4.png';
+
+
+
+var nametag = document.getElementById("nametag");
+var present = document.getElementById("present");
+var presentImage = document.getElementById("present-image");
+
+
+function init() {
+  
+  var _giftLink, 
+      _giftImg;
+  
+  if (gift_url) {
+    _giftLink = document.createElement("a");
+    _giftLink.href = gift_url;
+    _giftLink.target = "_blank";
+    presentImage.appendChild(_giftLink);
+  }
+  
+  if (gift_image_url) {
+    _giftImg = document.createElement("img");
+    _giftImg.src = gift_image_url;
+    if(_giftLink) {
+      _giftLink.appendChild(_giftImg);
+    } else {
+      presentImage.appendChild(_giftImg);
+    }
+  }
+    
+  present.addEventListener("click", function(e){
+    present.classList.toggle("open");
+    document.getElementById('card').classList.add('card-show')
+  }, false);
+  
+  
+  
+  nametag.innerText = to;
+}
+
+init();
+
